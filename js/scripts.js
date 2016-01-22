ccb8109864ba9bc2a203b4a86eebf91dfe5ff797
Jay Whang <jaywhang83@gmail.com>
2016-01-22 11:20:53 -0800

    Business log done

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
 function pingPong(userInputNumber) {
+  var numberBank = [];
   for (var index = 1; index <= userInputNumber; index++) {
     if (index % 15 === 0) {
       console.log("pingpong");
-      return "pingpong";
+      numberBank.push("pingpong");
     }
     else if(index % 3 === 0) {
       console.log("ping");
-      return "ping";
+      numberBank.push("ping");
     }
     else if (index% 5 === 0) {
       console.log("pong");
-      return "pong";
+      numberBank.push("pong");
     }
     else {
       console.log(index);
-      return index;
+      numberBank.push(index);
     }
   }
-
+  return numberBank;
 }
 
 $(document).ready(function(){
   $("form").submit(function(event){
     var userInputNumber = parseInt($("#number").val());
-    var numberHolder = " ";
-    for (var i = 1; i <= userInputNumber; i++) {
-      numberHolder +=pingPong(userInputNumber);
-
-    }
-    $("#result").append(numberHolder);
-
+    var numberHolder = pingPong(userInputNumber)
+    $("#result").empty();
+    console.log(numberHolder);
+    numberHolder.forEach(function(index){
+      $("#result").append("<li>" + index + "</li>");
+    });
     event.preventDefault();
   });
-  //event.preventDefault();
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,14 @@
 describe('pingPong', function() {
   it('it will print "pingpong" if userInputNumber is divisible by 15', function() {
-    expect(pingPong(15)).to.equal(1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong");
+    expect(pingPong(15)).to.eql([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]);
   })
   it('it will print "ping" if userInputNumber is divisible by 3', function() {
-    expect(pingPong(3)).to.equal(1, 2, "ping");
+    expect(pingPong(3)).to.eql([1, 2, "ping"]);
   });
   it('it will print "pong" if userInputNumber is divisible by 5', function() {
-    expect(pingPong(5)).to.equal(1, 2, "ping", 4, "pong");
+    expect(pingPong(5)).to.eql([1, 2, "ping", 4, "pong"]);
   });
   it('it will print 1, 2 if userInputNumber is not divisible by 3, 5, or 15', function() {
-    expect(pingPong(2)).to.equal(1, 2);
+    expect(pingPong(2)).to.eql([1, 2]);
   });
 });
