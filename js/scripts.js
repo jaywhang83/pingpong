d25158e748e0d7442aebc7b275eb9787e23d5fb2
Jay Whang <jaywhang83@gmail.com>
2016-01-22 09:55:54 -0800

    add divisible by 15 else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 function pingPong(userInputNumber) {
   for (var index = 1; index <= userInputNumber; index++) {
-    if(userInputNumber % 3 === 0) {
+    if (userInputNumber % 15 === 0) {
+      return "pingpong";
+    }
+    else if(userInputNumber % 3 === 0) {
       return "ping";
     }
     else if (userInputNumber % 5 === 0) {
-      return "pong"; 
+      return "pong";
     }
   }
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,12 @@
 describe('pingPong', function() {
+  it('it will print "pingpong" if userInputNumber is divisible by 15', function() {
+    expect(pingPong(15)).to.equal("pingpong");
+  })
   it('it will print "ping" if userInputNumber is divisible by 3', function() {
     expect(pingPong(3)).to.equal("ping");
   });
   it('it will print "pong" if userInputNumber is divisible by 5', function() {
     expect(pingPong(5)).to.equal("pong");
   })
+
 });
